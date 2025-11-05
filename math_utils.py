e71f204feb58f08485124a34364b83c2d34cf0e6
nareenz125-creator <nareenz125@gmail.com>
2025-11-05 13:03:02 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,18 +1,11 @@
 def find_max_number(num1, num2, num3):
-    if num1>num2 and num1>num3:
+    if num1>=num2 and num1>=num3:
       return num1
-    elif num2>num1 and num2>num3:
+    elif num2>=num1 and num2>=num3:
       return num2
-    elif num3>num1 and num3>num2 
+    else num3>=num1 and num3>=num2:
       return num3
-    elif num1==num2==num3:
-        return num1
-    elif num1==num2 or num1==num3:
-        return num1
-    elif num2==num3:
-        return num2
-    else:
-        return num3
+    
     
 def find_mean(num1, num2, num3):
     num4=(num1+num2+num3)/3
